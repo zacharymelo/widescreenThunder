d401ee1fdc591a4d8645472228b30f572117f58e
Zachary Melo <contact@zachmelo.com>
2013-07-17 11:16:49 -0400

    XML Create and Read

--- widescreenThunder/Form1.Designer.cs
+++ widescreenThunder/Form1.Designer.cs
@@ -34,7 +34,10 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.classStudents = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.classStudents)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -58,6 +61,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Check In";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -70,6 +74,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Check Up";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBox1
             // 
@@ -111,17 +116,39 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "for";
             // 
+            // classStudents
+            // 
+            this.classStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.classStudents.Location = new System.Drawing.Point(132, 336);
+            this.classStudents.Name = "classStudents";
+            this.classStudents.Size = new System.Drawing.Size(594, 150);
+            this.classStudents.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "read XML";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(865, 491);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.classStudents);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.classStudents)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -134,6 +161,8 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView classStudents;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- widescreenThunder/Form1.cs
+++ widescreenThunder/Form1.cs
@@ -16,5 +16,46 @@ namespace widescreenThunder
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //This does not sync with our students.xml that exists already
+            DataTable studentTable = new DataTable();
+            studentTable.TableName = "Student";
+
+            DataColumn studentsName = new DataColumn();
+            DataColumn studentsHours = new DataColumn();
+            DataColumn studentsID = new DataColumn();
+
+            studentTable.Columns.Add(studentsName);
+            studentTable.Columns.Add(studentsHours);
+            studentTable.Columns.Add(studentsID);
+
+            studentTable.Rows.Add("Ryan", "13", "01");
+            studentTable.Rows.Add("Ryan", "13", "02");
+            studentTable.Rows.Add("Ryan", "13", "03");
+
+            DataSet studentsDatabase = new DataSet();
+            studentsDatabase.DataSetName = "Students";
+            studentsDatabase.Tables.Add(studentTable);
+
+            studentsDatabase.WriteXml("students2.xml");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
